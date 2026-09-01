8323e339b1803f9e497d58cb1921254680e538fe
sagrika thakur <sagrikathakur68@gmail.com>
2026-09-01 14:32:47 +0530

    feat: implement createUser model to handle database insertion for new users

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,18 @@
-import pool from "../config/db";
+import pool from "../config/db.js";
 
 
 // create a user model//
+export const createUser = async (data) => {
+  const { name, email, password, role = 'user' } = data;
+  const result = await pool.query(
+    `
+    INSERT INTO users(name , email , password , role)
+    VALUES ($1 , $2 , $3 , $4)
+    RETURNING* ;
+
+    ` ,
+    [name, email, password, role]
+
+  )
+  return result.rows[0]
+}
